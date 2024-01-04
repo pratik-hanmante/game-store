8af8f680c91e24d7797ece27b48f8c32c45d717d
pratik-hanmante <pratikhanmante3@gmail.com>
2024-01-04 23:06:54 +0530

    added mapput and mappost endpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,82 @@
+using GameStore.Api.Entities;
+
+const string GetGameEndpointName = "GetGame";
+
+List<Game> games = new() {
+    new Game()
+    {
+        Id= 1,
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "https://placehold.co/100"
+    },
+    new Game()
+    {
+        Id= 2,
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 9, 30),
+        ImageUri = "https://placehold.co/100"
+    },
+    new Game()
+    {
+        Id= 3,
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 9, 27),
+        ImageUri = "https://placehold.co/100"
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
+
+
+
+app.MapGet("/games/{id}", (int id) => 
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if(game ==null) 
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(game);
+})
+.WithName("GetGame");
+
+// creating new game entry in our class
+app.MapPost("/games",(Game game) => 
+{
+    game.Id = games.Max(game => game.Id) + 1;
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName);
+});
+
+app.MapPut("/games/{id}", (int id,Game updatedGame) => {
+    Game? existingGame = games.Find(game => game.Id == id);
+
+    if(existingGame ==null) 
+    {
+        return Results.NotFound();
+    }
+existingGame.Name = updatedGame.Name;
+existingGame.Genre = updatedGame.Genre;
+existingGame.Price = updatedGame.Price;
+existingGame.ReleaseDate = updatedGame.ReleaseDate;
+existingGame.ImageUri = updatedGame.ImageUri;
+
+return Results.NoContent();
+
+});
 
 app.Run(); 
+
+
